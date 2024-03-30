f6909443dee6dbeb2306229fea5195a3101191a8
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-30 22:02:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const path = require('path');
 let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8088';
+const port = process.env.PORT || '46111';
 app.set('port', port);
 const server = http.createServer(app);
 
